ace12f42522855d1397b8f1012ccc950967ed0bf
mahikero <federicoparassarmiento@yahoo.com>
2015-12-14 15:20:40 -0500

    up

---
+++ AJAX_Test2/js/script_jquery.js
@@ -0,0 +1,18 @@
+﻿//Testing for Text File
+
+/*use selector to chose id*/
+//$('#update').load('data.txt');
+
+/*use selector to chose class*/
+//$('.update').load('data.txt');
+
+
+//Testing for JSON File
+$.getJSON('data.json', function (data) {
+    var output = '<ul>'
+    $.each(data, function (key, val) {
+        output += '<li>' + val.name + '</li>' 
+    });
+    output += '</ul>'
+    $('#update').append(output);
+});
