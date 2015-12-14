bda1e3c0126d37bb10b15f7971bd12f8269c6cc7
mahikero <federicoparassarmiento@yahoo.com>
2015-12-14 07:59:13 -0500

    up

--- AJAX_Test2/js/script.js
+++ AJAX_Test2/js/script.js
@@ -1,5 +1,6 @@
 ﻿
 var request = new XMLHttpRequest();
+/*
 request.open('GET', 'data.txt');
 request.onreadystatechange = function () {
     if ((request.readyState === 4) && (request.status === 200)) {
@@ -9,5 +10,36 @@ request.onreadystatechange = function () {
             modify[2].innerHTML = request.responseText;
     }
 }
+*/
+
+//Testing for xml usage
+/*
+request.open('GET', 'data.xml');
+request.onreadystatechange = function () {
+    if ((request.readyState === 4) && (request.status === 200)) {
+        console.log(request.responseXML.getElementsByTagName('name')[1].childNodes[0].nodeValue);
+
+        var items = request.responseXML.getElementsByTagName('name')
+        var output = '<ul>'
+        for (var i = 0; i < items.length; i++) {
+            output += '<li>' + items[i].firstChild.nodeValue + '</li>'
+        }
+        output += '</ul>'
+
+        document.getElementById('update').innerHTML = output;
+    }
+}
+*/
+
+//Testing for json usage
+request.open('GET', 'data.json');
+request.onreadystatechange = function () {
+    if ((request.readyState === 4) && (request.status === 200)) {
+        var items = JSON.parse(request.responseText);
+        console.log(items);
+    }
+}
+
+
 request.send();
     
