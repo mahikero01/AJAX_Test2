7e349ef31fdb724d128bb5eee7129840c43a6a00
mahikero <federicoparassarmiento@yahoo.com>
2015-12-14 11:13:15 -0500

    up

--- AJAX_Test2/js/script.js
+++ AJAX_Test2/js/script.js
@@ -40,16 +40,40 @@ request.onreadystatechange = function () {
 
 
 //Testing for json usage
-
+/*
 request.open('GET', 'data.json');
 request.onreadystatechange = function () {
     if ((request.readyState === 4) && (request.status === 200)) {
         var items = JSON.parse(request.responseText);
-        console.log(items);
-    
+
+        var output = '<ul>'
+        for (var key in items) {
+            output += '<li>' + items[key].name + '</li>';
+        }
+        output += '</ul>'
+
+        document.getElementById('update').innerHTML = output;
     }
 }
+*/
 
 
-request.send();
-    
+//Testing for event-driven ajax
+var myButton = document.getElementById('loadbutton');
+myButton.onclick = function () {
+    request.open('GET', 'data.json');
+    request.onreadystatechange = function () {
+        if ((request.readyState === 4) && (request.status === 200)) {
+            var items = JSON.parse(request.responseText);
+
+            var output = '<ul>'
+            for (var key in items) {
+                output += '<li>' + items[key].name + '</li>';
+            }
+            output += '</ul>'
+
+            document.getElementById('update').innerHTML = output;
+        }
+    }
+    request.send();
+}
