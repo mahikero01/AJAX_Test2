f3fae969b5162f5dd0e55378af98ee3c2a3eb875
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2015-12-14 14:06:19 +0800

    up

--- AJAX_Test2/js/script.js
+++ AJAX_Test2/js/script.js
@@ -4,8 +4,9 @@ request.open('GET', 'data.txt');
 request.onreadystatechange = function () {
     if ((request.readyState === 4) && (request.status === 200)) {
         var modify =
-            document.getElementById('update');
-            modify.innerHTML = request.responseText;
+            document.getElementsByTagName('ul')
+                    [1].getElementsByTagName('li');
+            modify[2].innerHTML = request.responseText;
     }
 }
 request.send();
